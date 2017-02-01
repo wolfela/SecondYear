409e25588bc3041a0a1d23dec506ff69b165d903
Timothy Choi <chin_fung.choi@kcl.ac.uk>
2017-02-01 00:48:31 +0000

    Reverse accidental template change commit

--- coolbeans/settings.py
+++ coolbeans/settings.py
@@ -53,22 +53,16 @@ ROOT_URLCONF = 'coolbeans.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.jinja2.Jinja2',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/jinja2')],
-        'APP_DIRS': True,
-        'OPTIONS': {'environment': 'coolbeans.jinja2.Environment',},
-    },
-    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-           ],
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
         },
     },
 ]
