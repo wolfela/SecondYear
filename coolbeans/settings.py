d688f98e474a0ed96a278befe82d85788997f83a
Timothy Choi <chin_fung.choi@kcl.ac.uk>
2017-02-07 13:47:02 +0000

    Disable admin site

--- coolbeans/settings.py
+++ coolbeans/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split("|")
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
