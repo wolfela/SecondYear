504f1fa871b6b1d39b0e717b6e0cb53dca511f30
ajeya.jog <ajeya.jog@kcl.ac.uk>
2017-02-08 10:12:49 +0000

    Added template of the user interface

---
+++ js/menuAnimation.js
@@ -0,0 +1,34 @@
+//Dickbutt
+$(document).ready(function() {
+
+    var $menuButton = $(".hamburger"),
+        $content = $(".container"),
+        $menu = $(".menu-content"),
+        $sidebar = $(".sidebar"),
+        $title = $(".title-bar-title"),
+        isOpen = false;
+
+    $(document).foundation();
+    $menuButton.click(function(){
+        $menuButton.prop("disabled",true);
+        if(isOpen){
+            $content.animate({left: "-=110px",opacity:1},350);
+            $menu.animate({opacity:0, 'z-index': -1},400);
+            $sidebar.animate({width: "-=110px"},350);
+            $title.animate({left: "-=110px"},350);
+            $menuButton.animate({'margin-left' : "10px"},250, function(){
+                $menuButton.toggleClass('cross');
+            });
+        }else{
+            $content.animate({left: "+=110px",opacity:0.5},350);
+            $menu.animate({opacity:1, 'z-index': 1},200);
+            $sidebar.animate({width: "+=110px"},350);
+            $title.animate({left: "+=110px"},350);
+            $menuButton.animate({'margin-left': "120px"},350, function(){
+                $menuButton.toggleClass('cross');
+            });
+        }
+        isOpen=!isOpen;
+        $menuButton.prop("disabled",false);
+    });
+});
