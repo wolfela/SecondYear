cbd6f11dc2171642376d53ed61bc2bb8178852b1
Timothy Choi <chin_fung.choi@kcl.ac.uk>
2017-02-09 00:10:33 +0000

    Add AttemptQuestionModel class for storing attempts of questions

--- coolbeans/app/models/attempt.py
+++ coolbeans/app/models/attempt.py
@@ -1,10 +1,12 @@
 from django.db.models import CASCADE
 from django.db.models import ForeignKey
+from django_mysql.models import DynamicField
 from safedelete.models import SafeDeleteMixin
 
+from coolbeans.app.models.base import TimeStampedModel
+from coolbeans.app.models.question import QuestionModel
 from coolbeans.app.models.quiz import QuizModel
 from coolbeans.app.models.user import UserModel
-from coolbeans.app.models.base import TimeStampedModel
 
 
 class AttemptModel(TimeStampedModel, SafeDeleteMixin):
@@ -13,3 +15,12 @@ class AttemptModel(TimeStampedModel, SafeDeleteMixin):
     """
     user_id = ForeignKey(UserModel, on_delete=CASCADE)
     quiz_id = ForeignKey(QuizModel, on_delete=CASCADE)
+
+
+class AttemptQuestionModel(TimeStampedModel, SafeDeleteMixin):
+    """
+    A model for storing an attempt for a question.
+    """
+    attempt_id = ForeignKey(AttemptModel, on_delete=CASCADE)
+    question_id = ForeignKey(QuestionModel, on_delete=CASCADE)
+    data = DynamicField()
