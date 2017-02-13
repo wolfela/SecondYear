2899863d7ec0c1e6ccbee0fccab9135236adf0e8
k1502709 <mehdi.ramezanzadeh@kcl.ac.uk>
2017-02-13 23:56:00 +0000

    Quiz word match - A word from the left side may only be linked from one on the right side. Linking two words where one is already linked to another will disconnect the previous link

--- js/quiz-word-matching.js
+++ js/quiz-word-matching.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 
 	// SETUP
+	var canvas = document.getElementById('my-canvas');
+	var context = canvas.getContext('2d');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context.lineWidth = 2;
+
+	var $canvas = $('#my-canvas');
+	var canvasOffset = $canvas.offset();
+	var offsetX = canvasOffset.left;
+	var offsetY = canvasOffset.top;
+
+	function draw(fromPos, toPos) {
+		context.beginPath();
+		context.moveTo(fromPos.left - offsetX, fromPos.top - offsetY);
+		context.lineTo(toPos.left - offsetX, toPos.top - offsetY);
+		context.stroke();
+	}
+
 	var wordPairs = [];
-	function Word(langA, langB) {
-						this.langA = langA;
-						this.langB = langB;
-					  };
+	function Word(langA, langB) { this.langA = langA; this.langB = langB };
 
 	wordPairs.push(new Word('wordA1', 'wordB1'));
 	wordPairs.push(new Word('wordA2', 'wordB2'));
@@ -37,6 +52,73 @@ $(document).ready(function() {
 		$buttonB.text(wordB);
 	}
 
+	// IN ACTION
+	var drawnBetween = [];	// to fill with Drawn objects for buttons to draw lines between
+	function Drawn(left, right) { this.left = left; this.right = right }
+	function refreshLines() {
+
+		context.clearRect(0, 0, $canvas.width(), $canvas.height());
+		for(i = 0; i < drawnBetween.length; i++) {
+			draw(drawnBetween[i].left, drawnBetween[i].right);		
+		}
+
+	}
+
+	function removeIfExists(pos) {
+		for(i = 0; i < drawnBetween.length; i++) {
+			var item = drawnBetween[i];');
+			if((item.left.left == pos.left && item.left.top == pos.top) || 
+				(item.right.left == pos.left && item.right.top == pos.top)) {			
+				drawnBetween.splice(i, 1);
+			}
+			
+		}
+	}
+
+	var langAregex = RegExp('langA');
+	var langBregex = RegExp('langB');
+	var $selectedButton = null;
+
+	$('.button').click(function() {
+		if($selectedButton != null) {
+			var firstButtonId = $selectedButton.attr('id');
+			var secondButtonId = $(this).attr('id');
+
+			// checks the ids of the buttons to see which side they are on
+			// if the second button is on the same side as the first button, the first becomes de-selected
+			//	without drawing
+			if(!((langAregex.test(firstButtonId) && langAregex.test(secondButtonId)) || 
+				(langBregex.test(firstButtonId) && langBregex.test(secondButtonId)))) {
+
+				var from = $selectedButton.position();
+				from.top += ($selectedButton.height() / 2);
+
+				var to = $(this).position();
+				to.top += ($(this).height() / 2);
+				
+
+				if(langAregex.test(firstButtonId)) {
+					from.left += $selectedButton.width();
+					removeIfExists(from);
+					removeIfExists(to);
+					drawnBetween.push(new Drawn(from, to));
+				} else {
+					to.left += $selectedButton.width();
+					removeIfExists(from);
+					removeIfExists(to);
+					drawnBetween.push(new Drawn(to, from));
+				}
+
+				refreshLines();
+		
+			}
+			$selectedButton = null;
+
+		} else {
+			$selectedButton = $(this);
+		}
+	});
+
 });
 
 function shuffle(array) {
@@ -48,6 +130,7 @@ function shuffle(array) {
     }
     return array;
 }
+
 
 
 
