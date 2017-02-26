c43cb1b7af2b715fb9ea8cdba613ea6ae2d5a281
Muhammad Hussain <muhammad.hussain@kcl.ac.uk>
2017-02-26 23:29:19 +0000

    fixed the bug. Basically I had previously chosen an easy way to delete the last field and it would delete all the content that was inputted by the user before pressing the button. But it is fixed now

--- js/mcq-creation-update.js
+++ js/mcq-creation-update.js
@@ -3,28 +3,16 @@ var amount = 4;
 $(document).ready(function() {
 	$('#addField').click(function() {
 		if(amount < 10){
-			addFields();			
+			$("#addAnswer").append("<input type=\"checkbox\" class=\"checkbox\"> <input type=\"text\" class=\"mcqinputbox\" placeholder=\"Type your answer in here\"></br>");
+			amount++;			
 		}
 	});
 	$('#removeField').click(function() {
 		if(amount > 4){
-			$("#addAnswer").empty();
+			$("#addAnswer input:last").remove();
+			$("#addAnswer input:last").remove();
+			$("#addAnswer br:last").remove();
 			amount--;
-			
-			for(i = 0; i < amount; i++){
-				addSingleField();
-			}	
 		}
 	});
 });
-
-function addFields(){
-	if(amount < 10){
-		$("#addAnswer").append("<input type=\"checkbox\" class=\"checkbox\"> <input type=\"text\" class=\"mcqinputbox\" placeholder=\"Type your answer in here\"></br>");
-		amount++;			
-	}
-}
-
-function addSingleField(){
-	$("#addAnswer").append("<input type=\"checkbox\" class=\"checkbox\"> <input type=\"text\" class=\"mcqinputbox\" placeholder=\"Type your answer in here\"></br>");			
-}
