8163d7f3bfeaa5f2d53c692670014a42117aa780
Adrian Abramowski <k1502676@kcl.ac.uk>
2017-03-10 17:23:06 +0000

    Generalised the methods in question.py and added DND into urls.py

--- coolbeans/app/views/question.py
+++ coolbeans/app/views/question.py
@@ -2,8 +2,8 @@ from django.views.generic import TemplateView
 from django.template import RequestContext
 
 from coolbeans.app.forms import MCQForm
+from coolbeans.app.forms import DNDForm
 from django.shortcuts import render
-
 
 
 class MCQCreateView(TemplateView):
@@ -12,33 +12,59 @@ class MCQCreateView(TemplateView):
     """
     template_name = "app/question/MCQ-Creation-update.html"
 
+    def previewMCQ(request):
+        preview(request, 'MCQ', MCQForm)
 
-    def preview(request):
-        """
-        Method for previewing the question
-        :return:
-        """
-        if request.method == 'POST':
-            form = MCQForm(request.POST)
-            if form.is_valid():
-                return render(request, 'app/question/MCQ-Display.html', {'form': form})
+    def saveMCQ(request):
+        save(request, 'MCQ', MCQForm)
+
+
+class DNDCreateView(TemplateView):
+    """
+    A view for creating Multiple Choice Questions
+    """
+    template_name = "app/question/DND-Creation.html"
+
+
+    def previewDND(request):
+        preview(request, 'DND', DNDForm)
+
+
+    def saveDND(request):
+        save(request, 'DND', DNDForm)
+
+
+def preview(request, type, formtype):
+    pathDisplay = 'app/question/' + type + '-Display.html'
+    """
+    Method for previewing the question
+    :return:
+    """
+    if request.method == 'POST':
+        form = formtype(request.POST)
+        if form.is_valid():
+            return render(request, pathDisplay, {'form': form})
+    else:
+        form = formtype()
+
+    return render(request, pathDisplay, {'form': form})
+
+
+def save(request, type, formtype):
+    pathCreation = 'app/question' + type + '-Creation.html'
+    pathType = '/' + type
+    """
+    Method for saving the question, right now its a preview as well, to be fixed
+    :return:
+    """
+    if request.method == 'POST':
+        form = formtype(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            return render(request, pathCreation, {'form': form})
         else:
-            form = MCQForm()
+            form = DNDForm()
 
-        return render(request, 'app/question/MCQ-Display.html', {'form': form})
+        return render(request, pathType, {'form': form},context_instance=RequestContext(request))
 
-    def save(request):
-        """
-        Method for saving the quuestion, right now its a preview as well, to be fixed
-        :return:
-        """
-        if request.method == 'POST':
-            form = MCQForm(request.POST)
-            if form.is_valid():
-                form.save(commit=False)
-                return render(request, 'app/question/MCQ-Display.html', {'form': form})
-        else:
-            form = MCQForm()
-
-        return render(request, '/mcq2', {'form': form},context_instance=RequestContext(request))
 
