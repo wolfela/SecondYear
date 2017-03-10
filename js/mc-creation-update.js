21992885c0150a565c642a71276d801ab9eb03b7
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-03-10 23:59:59 +0000

    Fixed multiple choice question naming and extra conflict code in forms.py

--- js/mc-creation-update.js
+++ js/mc-creation-update.js
@@ -3,7 +3,7 @@ var amount = 4;
 $(document).ready(function() {
 	$('#addField').click(function() {
 		if(amount < 10){
-			$("#addAnswer").append("<input type=\"checkbox\" class=\"checkbox\"> <input type=\"text\" class=\"mcqinputbox\" placeholder=\"Type your answer in here\"></br>");
+			$("#addAnswer").append("<input type=\"checkbox\" class=\"checkbox\"> <input type=\"text\" class=\"mcinputbox\" placeholder=\"Type your answer in here\"></br>");
 			amount++;			
 		}
 	});
