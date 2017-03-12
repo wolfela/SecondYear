19710f799ae277d92fbbcd8282736510226b8cc2
ajeya.jog <ajeya.jog@kcl.ac.uk>
2017-03-12 22:06:55 +0000

    Created crossword puzzle and updated gap fill

--- js/gapFill.js
+++ js/gapFill.js
@@ -1,61 +1,107 @@
-CKEDITOR.replace('gapFillInput', {
-    allowedContent: true,
-    removePlugins: 'elementspath'
-});
+var gaps = new Map(),
+    selectedRow,
+    selectedWord;
 
-function getSelected() {
-    return CKEDITOR.instances.gapFillInput.getSelection().getSelectedText();
+CKEDITOR.replace('gapFillInput');
+
+function getText() {
+    return CKEDITOR.instances.gapFillInput.getData();
 }
 
-function correctFormat(text){
-    var data = CKEDITOR.instances.gapFillInput.getData();
-    var html = '<span style="color:red">' + text.trim() + '</span>';
-    var pos = data.search(html)+html.length;
-    if(!/\s/.test(data.charAt(pos))){
-        CKEDITOR.instances.gapFillInput.setData([data.slice(0,pos),'&nbsp;',data.slice(pos)].join(''));
-    }
+function getSelected() {
+    return (document.all) ? document.selection.createRange().text : document.getSelection();
+}
 
+function loadAltAnswers(altAnswer) {
+    var altElement = $('<div class=gapFillAltRow tabindex=1>'+altAnswer+'</div>').appendTo($('.gapFillAltAnswers'));
+    $(altElement).click(function () {
+        selectedRow = this;
+        console.log('yaya')
+    })
+}
+
+function addOption(content) {
+    var option = '<option value='+content+'>'+content+'</option>';
+    $('.gapFillAnswerOptions').append(option);
+    if($('.gapFillAnswerOptions').children().length==1){
+        selectedWord=content;
+    }
 }
 
 function addGap(){
-    var text = getSelected();
+    var selected = getSelected();
+    var text = selected.toString();
     if(text.length!=0) {
-        var editor = CKEDITOR.instances.gapFillInput;
-        if (editor.mode == 'wysiwyg') {
-            editor.insertHtml('<span style="color:red">' + text + '</span>');
-            correctFormat(text)
-        } else {
-            alert("WYSIWYG!");
-        }
+        var highlight = document.createElement('SPAN');
+        var range = selected.getRangeAt(0);
+        highlight.textContent = text;
+        highlight.style.cssText = 'color:red';
+        range.deleteContents();
+        range.insertNode(highlight);
+        var altAnswer = [];
+        gaps.set(text,altAnswer);
+        addOption(text);
     }
+}
+
+function getSelectedElement() {
+    var containerElement = null;
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var node = sel.getRangeAt(0).commonAncestorContainer;
+            containerElement = node.nodeType == 1 ? node : node.parentNode;
+        }
+    } else if (typeof document.selection != "undefined" &&
+        document.selection.type != "Control") {
+        var textRange = document.selection.createRange();
+        containerElement = textRange.parentElement();
+    }
+    return containerElement
 }
 
 function deleteGap() {
-    var text = getSelected();
-    if(text.length!=0) {
-        var selection = getSelected().trim();
-        var editor = CKEDITOR.instances.gapFillInput;
-        editor.insertHtml(selection);
+    var selected = getSelectedElement();
+    if($(selected).is('span')) {
+        $(selected).replaceWith(getSelected().toString())
+    }else{
+        alert('Be more careful selecting pls(ie dont select spaces')
     }
 }
 
-function removeHTML(html){
-    for(var i =0;i<html.length;i++){
-        html[i] = html[i].replace(/<[^>]*>/g,"")
+function getChange(select) {
+    selectedWord = select.value;
+    $('.gapFillAltAnswers').empty();
+    var altWords = gaps.get(selectedWord);
+    for(var i=0;i<altWords.length;i++){
+        loadAltAnswers(altWords[i])
     }
+    $('.gapFillAltInput').val('');
 }
 
-function createQuiz() {
-    var editor = CKEDITOR.instances.gapFillInput;
-    var data = editor.getData();
-    //Gets text that has gaps in it
-    var spanless = data.split(/<span[^>]*>[^>]*<\/span>/g);
-    removeHTML(spanless);
-    //Get gaps
-    var gaps = [];
-    var gap = /<span[^>]*>[^>]*<\/span>/g.exec(data);
-    if(gap!=null) {
-        gaps.push(gap.toString().replace(/<\/?span[^>]*>/g, ''));
-    }
-    displayQuiz(gaps,spanless);
+function addAltGap() {
+    var altAnswer = $('.gapFillAltInput').val();
+    gaps.get(selectedWord).push(altAnswer);
+    $('.gapFillAltAnswers').append('<p>'+altAnswer+'</p>')
+    $('.gapFillAltInput').val('');
+}
+
+function deleteAltGap() {
+    // gaps.get(selectedWord).splice(gaps.get(selectedWord).indexOf($(selectedRow).val()),1);
+    console.log($(selectedRow).val());
+    $(selectedRow).remove();
+}
+
+function closeCKEDITOR(){
+    $('.gapFillSelectBox').append('<p>'+ getText() + '</p>');
+    CKEDITOR.instances.gapFillInput.setReadOnly(true);
+    $('#cke_gapFillInput, .gapFillNext').css({'opacity': '0.3','pointer-events':'none'});
+    $('.gapFillSelectBox, .gapFillBack').css({'opacity': '1','pointer-events':'auto'});
+}
+
+function openCKEDITOR() {
+    $('#cke_gapFillInput, .gapFillNext').css({'opacity': '1','pointer-events':'auto'});
+    $('.gapFillSelectBox, .gapFillBack').css({'opacity': '0.3','pointer-events':'none'});
+    $('.gapFillSelectBox').empty();
+    CKEDITOR.instances.gapFillInput.setReadOnly(false);
 }
