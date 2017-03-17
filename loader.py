3bd2409981c754dd8c4511e13f7b202ee97ed6a8
Thomas Perkins <thomas.d.perkins@kcl.ac.uk>
2017-03-17 01:52:46 +0000

    Templated Results and Word Match pages

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('jMCQ-Creation-update.html', content="?",)
+    return render_template('jResults.html', content="?",)
 
 
 if (__name__ == '__main__'):
