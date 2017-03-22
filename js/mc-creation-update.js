a7cbf7c2d85852bb9da67b85c944c374803c881d
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-03-22 22:48:59 +0000

    Added answer check to multiple choice questions

--- js/mc-creation-update.js
+++ js/mc-creation-update.js
@@ -1,6 +1,8 @@
 var amount = 4;
 
 $(document).ready(function() {
+
+
 	$('#addField').click(function() {
 		if(amount < 10){
 			$("#addAnswer").append(" <input type=\"text\" class=\"mcinputbox\" name=\"answers[]\" placeholder=\"Type your answer in here\">");
@@ -13,11 +15,39 @@ $(document).ready(function() {
 			$("#addAnswer input:last").remove();
 			amount--;
 		}
+
+
 	});
 
 
 
 
+
+$("#check").click(function () {
+
+		var answer = document.querySelector('input[name="answers"]:checked').value;
+		var correct = document.getElementById("correct").value;
+		var id_checkbox = $('input[type=radio][name="answers"]:checked').attr('id');
+		var id = id_checkbox.split("-"); 
+		var id_text = id[1];
+
+		$('#choices').children('input').each(function () {
+			if(this.value!=answer){
+				this.disabled=true;
+			}
+		});
+
+
+		if(correct==answer){
+			var field = document.getElementById(id_text);
+			$(field).addClass('right');
+		}
+		else{
+			var field = document.getElementById(id_text);
+			$(field).addClass('wrong');
+		}
+
+});
 
 
 // $('#post-form').on('submit', function(){
