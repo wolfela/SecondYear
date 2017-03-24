a63d64bf88b56f2a7f8147660b78255f2b52e71d
Thomas Perkins <thomas.d.perkins@kcl.ac.uk>
2017-03-24 00:16:01 +0000

    Improved Quiz Find and Quiz Id

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('jResults.html', content="?",)
+    return render_template('Quiz-ID.html', content="?",)
 
 
 if (__name__ == '__main__'):
