b2ca97b178dba94fa3a0b1e7ddec44f2dfe46da3
Thomas Perkins <thomas.d.perkins@kcl.ac.uk>
2017-03-24 13:34:53 +0000

    Centered text in in Quiz-Find, and added button to create quiz

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('Quiz-ID.html', content="?",)
+    return render_template('Quiz-Find.html', content="?",)
 
 
 if (__name__ == '__main__'):
