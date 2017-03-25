a0e1d7d33636925822f97c32c4f3bce988ca4a1a
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-03-25 22:03:25 +0000

    Deleted unused models, commented everything

--- coolbeans/app/models/__init__.py
+++ coolbeans/app/models/__init__.py
@@ -1,7 +1,7 @@
 # Discoverable model imports
-from coolbeans.app.models.question import BaseQuestionModel, MultipleChoiceModel, PlaceholderQuestionModel
+from coolbeans.app.models.question import BaseQuestionModel, MultipleChoiceModel, WordMatchingModel, WordScrambleQuestionModel, CrosswordQuestionModel, GapFillQuestionModel
 from coolbeans.app.models.quiz import QuizModel
 from coolbeans.app.models.user import UserModel
 
 
-__all__ = ['BaseQuestionModel', 'MultipleChoiceModel', 'PlaceholderQuestionModel', 'QuizModel', 'UserModel']
+__all__ = ['BaseQuestionModel', 'MultipleChoiceModel', 'WordMatchingModel', 'WordScrambleQuestionModel', 'CrosswordQuestionModel', 'GapFillQuestionModel']
