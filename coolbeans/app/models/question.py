4505b14403d1029f5fa9378c7f6f638c153b196e
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-03-25 21:44:48 +0000

    Changed word scramble to random order instead of manual field

--- coolbeans/app/models/question.py
+++ coolbeans/app/models/question.py
@@ -193,7 +193,6 @@ class WordScrambleQuestionModel(BaseQuestionModel):
     """
     title = CharField(max_length=500)
     answer = CharField(max_length=255)
-    scrambled_sentence = CharField(max_length=500, blank = False)
     score = PositiveIntegerField(blank=True, default=1, null=True)
     quiz = CharField(max_length=500, blank=False)
     position = CharField(max_length=500, blank=False)
